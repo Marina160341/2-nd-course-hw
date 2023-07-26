97578932c2bcf5c9ef3ff85d5f6b43a304a73a96
Marina160341 <kemova-marina@rambler.ru>
2023-07-26 14:33:39 +0300

    Домашнее задание №8 (повторно)

--- hw-8.js
+++ hw-8.js
@@ -11,56 +11,52 @@ console.log(people);
 
 //Задание 2
 
-function isPositive(number) {
-   return number>0;
-    }
-          
-    function filter(arr, item) {
-        const output = [];
-        for (let i = 0; i < arr.length; i++) {
-            if (item(arr[i])) {
-                output.push(arr[i]);
+ function isPositive(number) {
+      return number>0;
+   }
+ function isMale(item) { 
+      return item.gender == 'male';
+}
+function filter(arr, item) {
+    const output = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (item(arr[i])) {
+        output.push(arr[i]);
             }
         }
         return output;
-    }
+     }
+     const people2 = [
+         {name: 'Глеб', gender: 'male'},
+         {name: 'Анна', gender: 'female'},
+         {name: 'Олег', gender: 'male'},
+         {name: 'Оксана', gender: 'female'}
+      ];
    
-    console.log(filter([3, -4, 1, 9], isPositive));
+     console.log(filter(people2, isMale));
+     console.log(filter([3, -4, 1, 9], isPositive));
     
-    const people1 = [
-        {name: 'Глеб', gender: 'male'},
-        {name: 'Анна', gender: 'female'},
-        {name: 'Олег', gender: 'male'},
-        {name: 'Оксана', gender: 'female'}
-     ];
-     let genderPeople =  people1.filter(function(item) {
-        return item.gender == 'male'});
-        console.log(genderPeople);
-//Задание 3
-let currentDate=new Date();
-let timerId = setInterval(() => console.log(`${currentDate}`), 3000);       
-setTimeout(() => { clearInterval(timerId); console.log('30 секунд прошло'); }, 30000);    
-    
-//Задание 4
-function delayForSecond(callback) {
-    console.log( callback );
-  }
+//Задание 3    
+let timerId = setInterval(() => { console.log(new Date);}, 3000);
+setTimeout(() => { clearInterval(timerId); console.log('30 секунд прошли'); }, 30000);
   
-  setTimeout(delayForSecond, 1000, "Привет, Василий!"); 
+  
+//Задание 4
+   function delayForSecond() {
+     console.log('Привет,Глеб');
+   }
+   setTimeout(delayForSecond, 1000);
 
 // Задание 5 
-  
 function delayForSecond2(cb) {
     setTimeout(() => {
         console.log('Прошла одна секунда');
 				if(cb) { 	cb(); }
+
     }, 1000)
 }
-
 function sayHi (name) {
     console.log(`Привет, ${name}!`);
 }
-console.log(delayForSecond2());
-setTimeout(sayHi,1000,"Глеб!");
-    
-    
+delayForSecond2(() => sayHi('Глеб'));
+  
